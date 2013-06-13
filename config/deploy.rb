ab8fe98f4aaa49542c6138b96dd0246dacd0d9e1
Emeka Ahamba <emeka.ahamba@ucitech.com.ng>
2013-06-13 16:53:59 +0100

    adding config files

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ server "VM2", :web, :app, :db, primary: true
 set :deploy_to, "/home/#{user}/apps/#{application}"
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
-ssh_options[:port] = 22
+ssh_options[:port] = 80
 
 
 namespace :deploy do
